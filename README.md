b42517f5f176008968339db197e70fdaadba435e
Murat Karagozgil <murat.karagozgil@inomera.com>
2020-02-16 19:43:02 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+## Running
+To run project:
+```
+./gradlew build
+java -jar build/libs/shopping-cart-test-project-1.0.jar
+```
+
+To run all tests:
+```
+./gradlew test
+```
+
+## Rest
+Swagger Url is `http://localhost:8080/shopping-cart-test-project`
+
+## Postman Collection
+
--- src/main/java/com/shopping/cart/controller/DataInitController.java
+++ src/main/java/com/shopping/cart/controller/DataInitController.java
@@ -6,7 +6,6 @@ import com.shopping.cart.repository.*;
 import com.shopping.cart.service.ShoppingCartService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,7 +18,7 @@ import java.math.BigDecimal;
  * @author Murat Karagözgil
  */
 
-@RequestMapping("/initDefaultData")
+@RequestMapping("/init-default-data")
 @RestController
 public class DataInitController {
 
@@ -41,131 +40,131 @@ public class DataInitController {
     @Autowired
     private ShoppingCartService shoppingCartService;
 
-    @GetMapping(value = "/onlyParamData", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
-    ResponseEntity<Boolean> initOnlyParamData() {
-        saveOnlyParamData();
+    @GetMapping(value = "/only-parameters")
+    public ResponseEntity<Boolean> initOnlyParamData() {
+        insertOnlyProducts();
         return new ResponseEntity<>(Boolean.TRUE, HttpStatus.OK);
     }
 
-    @GetMapping(value = "/initFullData", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
-    ResponseEntity<Boolean> initFullData() {
-        saveOnlyParamData();
-        saveCartData();
+    @GetMapping(value = "/init-full-data")
+    public ResponseEntity<Boolean> initFullData() {
+        insertOnlyProducts();
+        addProductsToCart();
         return new ResponseEntity<>(Boolean.TRUE, HttpStatus.OK);
     }
 
     @Transactional
-    public void saveOnlyParamData() {
-        Category category1 = Category.builder().title("Bilgisayar").build();
+    public void insertOnlyProducts() {
+        Category category1 = Category.builder().title("Computer").build();
         category1 = categoryRepository.save(category1);
 
-        Category category2 = Category.builder().title("Telefon").build();
+        Category category2 = Category.builder().title("Phone").build();
         category2 = categoryRepository.save(category2);
 
-        Category category3 = Category.builder().title("Kitap").build();
+        Category category3 = Category.builder().title("Book").build();
         category3 = categoryRepository.save(category3);
 
-        Category category4 = Category.builder().title("Araba").build();
+        Category category4 = Category.builder().title("Car").build();
         category4 = categoryRepository.save(category4);
 
 
-        Product product1 = Product.builder().category(category1).title("Lenovo v130").price(new BigDecimal("1500")).build();
+        Product product1 = Product.builder().category(category1).title("Lenovo v130").price(new BigDecimal(1500)).build();
         productRepository.save(product1);
 
-        Product product2 = Product.builder().category(category1).title("Acer A315").price(new BigDecimal("1900")).build();
+        Product product2 = Product.builder().category(category1).title("Acer A315").price(new BigDecimal(1000)).build();
         productRepository.save(product2);
 
-        Product product3 = Product.builder().category(category1).title("Asus X540").price(new BigDecimal("2400")).build();
+        Product product3 = Product.builder().category(category1).title("Asus X540").price(new BigDecimal(2300)).build();
         productRepository.save(product3);
 
-        Product product4 = Product.builder().category(category1).title("HP 250").price(new BigDecimal("4000")).build();
+        Product product4 = Product.builder().category(category1).title("HP 250").price(new BigDecimal(5000)).build();
         productRepository.save(product4);
 
-        Product product5 = Product.builder().category(category1).title("Lenovo IP330").price(new BigDecimal("2500")).build();
+        Product product5 = Product.builder().category(category1).title("Lenovo IP330").price(new BigDecimal(3500)).build();
         productRepository.save(product5);
 
-        Product product6 = Product.builder().category(category1).title("Apple Macbook Air").price(new BigDecimal("6000")).build();
+        Product product6 = Product.builder().category(category1).title("Apple Macbook Air").price(new BigDecimal(7000)).build();
         productRepository.save(product6);
 
-        Product product7 = Product.builder().category(category2).title("Xiaomi Redmi Note 8").price(new BigDecimal("1820")).build();
+        Product product7 = Product.builder().category(category2).title("Xiaomi Redmi Note 8").price(new BigDecimal(2320)).build();
         productRepository.save(product7);
 
-        Product product8 = Product.builder().category(category2).title("Huawei Y6").price(new BigDecimal("1100")).build();
+        Product product8 = Product.builder().category(category2).title("Huawei Y6").price(new BigDecimal(1200)).build();
         productRepository.save(product8);
 
-        Product product9 = Product.builder().category(category2).title("Samsung Galaxy A2").price(new BigDecimal("700")).build();
+        Product product9 = Product.builder().category(category2).title("Samsung Galaxy A2").price(new BigDecimal(750)).build();
         productRepository.save(product9);
 
-        Product product10 = Product.builder().category(category2).title("iPhone 11").price(new BigDecimal("7000")).build();
+        Product product10 = Product.builder().category(category2).title("iPhone 11").price(new BigDecimal(9000)).build();
         productRepository.save(product10);
 
-        Product product11 = Product.builder().category(category2).title("Samsung Note 10").price(new BigDecimal("10000")).build();
+        Product product11 = Product.builder().category(category2).title("Samsung Note 10").price(new BigDecimal(12000)).build();
         productRepository.save(product11);
 
-        Product product12 = Product.builder().category(category2).title("Huawei P20 Lite").price(new BigDecimal("1500")).build();
+        Product product12 = Product.builder().category(category2).title("Huawei P20 Lite").price(new BigDecimal(2000)).build();
         productRepository.save(product12);
 
-        Product product13 = Product.builder().category(category3).title("Karamazov Kardeşler").price(new BigDecimal("23")).build();
+        Product product13 = Product.builder().category(category3).title("Dracula").price(new BigDecimal(30)).build();
         productRepository.save(product13);
 
-        Product product14 = Product.builder().category(category3).title("Satranç").price(new BigDecimal("7")).build();
+        Product product14 = Product.builder().category(category3).title("1984").price(new BigDecimal(9)).build();
         productRepository.save(product14);
 
-        Product product15 = Product.builder().category(category3).title("Suç ve Ceza").price(new BigDecimal("15")).build();
+        Product product15 = Product.builder().category(category3).title("Harry Potter").price(new BigDecimal(18)).build();
         productRepository.save(product15);
 
-        Product product16 = Product.builder().category(category3).title("Kumarbaz").price(new BigDecimal("10")).build();
+        Product product16 = Product.builder().category(category3).title("The Outsiders").price(new BigDecimal(12)).build();
         productRepository.save(product16);
 
-        Product product17 = Product.builder().category(category3).title("İlyada").price(new BigDecimal("25")).build();
+        Product product17 = Product.builder().category(category3).title("Bachbeth").price(new BigDecimal(30)).build();
         productRepository.save(product17);
 
-        Product product18 = Product.builder().category(category3).title("Aforizmalar").price(new BigDecimal("8")).build();
+        Product product18 = Product.builder().category(category3).title("Robinson Crusoe").price(new BigDecimal(10)).build();
         productRepository.save(product18);
 
-        Product product19 = Product.builder().category(category4).title("Mercedes C-180").price(new BigDecimal("194000")).build();
+        Product product19 = Product.builder().category(category4).title("Mercedes C-180").price(new BigDecimal(264000)).build();
         productRepository.save(product19);
 
-        Product product20 = Product.builder().category(category4).title("Mini Cooper").price(new BigDecimal("14000")).build();
+        Product product20 = Product.builder().category(category4).title("Mini Cooper").price(new BigDecimal(150000)).build();
         productRepository.save(product20);
 
-        Product product21 = Product.builder().category(category4).title("Audi Q7").price(new BigDecimal("510000")).build();
+        Product product21 = Product.builder().category(category4).title("Audi Q7").price(new BigDecimal(600000)).build();
         productRepository.save(product21);
 
-        Product product22 = Product.builder().category(category4).title("Opel Astra").price(new BigDecimal("56000")).build();
+        Product product22 = Product.builder().category(category4).title("Opel Astra").price(new BigDecimal(90000)).build();
         productRepository.save(product22);
 
-        Product product23 = Product.builder().category(category4).title("Volvo S80").price(new BigDecimal("14000")).build();
+        Product product23 = Product.builder().category(category4).title("Volvo S80").price(new BigDecimal(200000)).build();
         productRepository.save(product23);
 
-        Product product24 = Product.builder().category(category4).title("BMW 5.20").price(new BigDecimal("190000")).build();
+        Product product24 = Product.builder().category(category4).title("BMW 5.20").price(new BigDecimal(550000)).build();
         productRepository.save(product24);
 
 
-        Campaign campaign1 = Campaign.builder().title("%20 İndirim").category(category1).discountAmount(new BigDecimal("20"))
+        Campaign campaign1 = Campaign.builder().title("50% Discount").category(category1).discountAmount(new BigDecimal(50))
                 .itemLimit(3).discountType(DiscountType.RATE).build();
         campaignRepository.save(campaign1);
 
-        Campaign campaign2 = Campaign.builder().title("%50 İndirim").category(category2).discountAmount(new BigDecimal("50"))
+        Campaign campaign2 = Campaign.builder().title("%30 Discount").category(category2).discountAmount(new BigDecimal(30))
                 .itemLimit(5).discountType(DiscountType.RATE).build();
         campaignRepository.save(campaign2);
 
-        Campaign campaign3 = Campaign.builder().title("5 TL İndirim").category(category3).discountAmount(new BigDecimal("5"))
+        Campaign campaign3 = Campaign.builder().title("5$ Discount").category(category3).discountAmount(new BigDecimal(5))
                 .itemLimit(5).discountType(DiscountType.AMOUNT).build();
         campaignRepository.save(campaign3);
 
 
-        Coupon coupon1 = Coupon.builder().title("%10 Kupon İndirim").discountAmount(new BigDecimal("10")).minPurchaseAmount(new BigDecimal(100))
+        Coupon coupon1 = Coupon.builder().title("10% Coupon Discount").discountAmount(new BigDecimal(10)).minPurchaseAmount(new BigDecimal(100))
                 .discountType(DiscountType.RATE).build();
         couponRepository.save(coupon1);
 
-        Coupon coupon2 = Coupon.builder().title("50 TL Kupon İndirim").discountAmount(new BigDecimal("50")).minPurchaseAmount(new BigDecimal(20))
+        Coupon coupon2 = Coupon.builder().title("50$ Coupon Discount").discountAmount(new BigDecimal(50)).minPurchaseAmount(new BigDecimal(20))
                 .discountType(DiscountType.AMOUNT).build();
         couponRepository.save(coupon2);
     }
 
     @Transactional
-    public void saveCartData() {
+    public void addProductsToCart() {
         ShoppingCart shoppingCart = new ShoppingCart();
         shoppingCart = shoppingCartRepository.save(shoppingCart);
         Long id = shoppingCart.getId();
@@ -188,7 +187,7 @@ public class DataInitController {
         shoppingCartService.applyCoupon(id, 32L);
 
 
-        DeliveryCostDTO deliveryCostDTO = new DeliveryCostDTO(new BigDecimal("2"), new BigDecimal("2"));
+        DeliveryCostDTO deliveryCostDTO = new DeliveryCostDTO(new BigDecimal(2), new BigDecimal(2));
         shoppingCartService.applyDelivery(id, deliveryCostDTO);
     }
 
